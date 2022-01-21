b8028412e2a25cb45abbbe4b1c984a9b2624833d
Mat Ivan Arquero <mativanarquero@gmail.com>
2022-01-21 14:07:34 +0800

    Change button and wallet modal imports to sparkswap-ui kit

--- src/components/UnlockButton.tsx
+++ src/components/UnlockButton.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Button, useWalletModal } from '@pancakeswap/uikit'
+import { Button, useWalletModal } from '@sparkpointio/sparkswap-uikit'
 import useAuth from 'hooks/useAuth'
 import { useTranslation } from 'contexts/Localization'
 
 const UnlockButton = (props) => {
   const { t } = useTranslation()
   const { login, logout } = useAuth()
-  const { onPresentConnectModal } = useWalletModal(login, logout, t)
+  const { onPresentConnectModal } = useWalletModal(login, logout)
 
   return (
     <Button onClick={onPresentConnectModal} {...props} fullWidth>
